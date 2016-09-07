d6e4a973e8c42e7f868fa011d208b756794665bb
MXD <MXD@MXD.local>
2016-09-07 11:27:36 -0600

    update2

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api, api');
 
-app.post('/api, api');
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
